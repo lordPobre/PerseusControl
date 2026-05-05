9554dfacde0b4f5b9bb9fc33345e0a51a4afa579
Carlos Lopez <clopez.ti@vcchile.cl>
2026-05-05 00:10:56 -0400

    correcciones

--- config/settings.py
+++ config/settings.py
@@ -34,8 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
     # Celery Beat (tareas programadas)
     'django_celery_beat',
