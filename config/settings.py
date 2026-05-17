ee4a220e83fbfdba2388974bcde639f53bf39696
Carlos Lopez <clopez.ti@vcchile.cl>
2026-05-17 18:03:43 -0400

    fix: ACL None, usar política bucket S3

--- config/settings.py
+++ config/settings.py
@@ -128,10 +128,13 @@ AWS_S3_REGION_NAME     = config('AWS_S3_REGION_NAME',     default='sa-east-1')
 if AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY and AWS_STORAGE_BUCKET_NAME:
     DEFAULT_FILE_STORAGE  = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL       = 'public-read'
+    AWS_DEFAULT_ACL       = None
     AWS_QUERYSTRING_AUTH  = False
     AWS_S3_CUSTOM_DOMAIN  = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
     MEDIA_URL             = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
 else:
     MEDIA_URL  = '/media/'
     MEDIA_ROOT = BASE_DIR / 'media'
